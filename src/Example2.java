adfd5a88bf11e7a88869c726dcd70f521e459b79
MarinaGMS <gutsalenko.marina@gmail.com>
2015-01-20 14:43:33 +0200

    add new project

---
+++ src/Example2.java
@@ -0,0 +1,14 @@
+/*Это еще один короткий пример.
+Назовите этот файл Example2.java
+* */
+
+class Example2{
+    public static void main(String args[]){
+        int num; //эта строка объявляет переменную по имени num
+        num = 100; //эта строка присваивает переменной num значение 100
+        System.out.println("Эта переменная num: " + num);
+        num = num *2;
+        System.out.print("Значение прееменной num * 2 равно");
+        System.out.println(num);
+    }
+}
