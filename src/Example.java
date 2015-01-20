a2dc5f5e1b33c3635c861853dd9b749c4ed2e92a
MarinaGMS <gutsalenko.marina@gmail.com>
2015-01-20 11:11:43 +0200

    add!!

--- src/Example.java
+++ src/Example.java
@@ -5,6 +5,6 @@ public class Example {
 
     //program started with main()
     public static void main(String args[]){
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
