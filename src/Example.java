3edbd3ad1f5d81637e7f1cb3867280ae17af3f21
MarinaGMS <gutsalenko.marina@gmail.com>
2015-01-20 10:38:22 +0200

    create project

---
+++ src/Example.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mgutsalenko on 20.01.2015.
+ */
+public class Example {
+
+    //program started with main()
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+}
